fe1dc866a031363aaf9137bf3c1bdecd3b831393
berk cirak <atalayberkcrak@gmail.com>
2025-09-20 13:37:36 +0300

    feat: Created ClientHandler

--- client-server-command/src/main/java/org/example/ClientHandler.java
+++ client-server-command/src/main/java/org/example/ClientHandler.java
@@ -1,2 +1,19 @@
-package org.example;public class ClientHandler {
+package org.example;
+
+import java.net.Socket;
+
+public class ClientHandler implements Runnable{
+
+    private Socket clientSocket;
+    private CommandServer commandServer;
+
+    public ClientHandler(Socket clientSocket, CommandServer commandServer){
+        this.commandServer=commandServer;
+    }
+
+
+    @Override
+    public void run() {
+
+    }
 }
